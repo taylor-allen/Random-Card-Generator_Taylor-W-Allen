1fddb6dd51b80f46759d14554ec60b2ab43c7dc5
Taylor Allen <50384820+taylor-allen@users.noreply.github.com>
2023-12-21 19:48:52 +0000

    12-21-23 commit

--- src/app.js
+++ src/app.js
@@ -1,11 +1,60 @@
 /* eslint-disable */
-import "bootstrap";
 import "./style.css";
 
-import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+document.body.style.backgroundColor = "green";
+
+let generateRandomNumber = () => {
+  let numbers = [
+    "A",
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8",
+    "9",
+    "10",
+    "J",
+    "Q",
+    "K"
+  ];
+  let indexNumbers = Math.floor(Math.random() * numbers.length);
+  return numbers[indexNumbers];
+};
+
+let generateRandomSuit = () => {
+  let suit = ["diamond", "spade", "heart", "club"];
+  let indexSuit = Math.floor(Math.random() * suit.length);
+  return suit[indexSuit];
+};
+
+let generateCard = () => {
+  const cardElement = document.querySelector(".card");
+
+  if (cardElement) {
+    const randomSuit = generateRandomSuit();
+    const randomNumber = generateRandomNumber();
+
+    // Remove previous suit class
+    cardElement.classList.remove("diamond", "spade", "heart", "club");
+
+    // Add new suit class
+    // Need this because it wasn't changing suits
+    cardElement.classList.add(randomSuit);
+
+    cardElement.innerHTML = randomNumber;
+  } else {
+    console.error("Element with class 'card' not found.");
+  }
+};
+
+window.onload = () => {
+  generateCard();
+
+  document.querySelector("#btn").addEventListener("click", () => {
+    generateCard();
+  });
 };
